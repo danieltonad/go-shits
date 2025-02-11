6bce3599aea9412677f117a0900d849d66a643fd
Daniel Solarin <soladan2000@gmail.com>
2025-02-11 04:12:35 -0900

    feat: add simple_read_type_conversion function for rating input and conversion

--- hello-go/main.go
+++ hello-go/main.go
@@ -4,13 +4,36 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
-func main() {
+func simple_read() {
 	welcome_msg := "Welcome Pussy"
 	fmt.Println(welcome_msg)
 	age_reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter your age: ")
 	age, _ := age_reader.ReadString('\n')
 	fmt.Println("You are", age, "years old")
+}
+
+func simple_read_type_conversion() {
+	welcome_msg := "Welcome To Ekpon Store!"
+	fmt.Println(welcome_msg)
+
+	rate_reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter the rate: ")
+	rating, _ := rate_reader.ReadString('\n')
+	// fmt.Println("The rating is", rating)
+
+	rating_no, err := strconv.ParseInt(rating, 10, 64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("The rating is", rating_no)
+	}
+}
+
+func main() {
+	// simple_read()
+	simple_read_type_conversion()
 }
