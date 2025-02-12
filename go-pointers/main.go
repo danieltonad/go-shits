d691ac11cb5f6f1d8be8905d5f515653124fb05e
Daniel Solarin <soladan2000@gmail.com>
2025-02-12 09:51:41 -0900

    feat: modify pointer value in main function to demonstrate value manipulation

--- go-pointers/main.go
+++ go-pointers/main.go
@@ -6,6 +6,8 @@ func main() {
 	fmt.Println("Pointers in Go")
 	myNumber := 24
 	var ptr = &myNumber
+	*ptr = *ptr * 10
 	fmt.Println("Pointer Address: ", ptr)
 	fmt.Println("Pointer Value: ", *ptr)
+	fmt.Println("No: ", myNumber)
 }
