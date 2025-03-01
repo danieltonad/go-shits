82cff318167f630aad934b716a6fde0e750030b9
Daniel Solarin <soladan2000@gmail.com>
2025-03-01 10:35:31 -0900

    bbbbbbb

--- fileio/main.go
+++ fileio/main.go
@@ -2,33 +2,53 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"io/ioutil"
 	"os"
 )
 
-func readFile() {
-	databyte, err := ioutil.ReadFile("file.txt")
+type User struct {
+	Name    string
+	Age     int
+	Address string
+}
+
+func readFile(path string) {
+	databyte, err := ioutil.ReadFile(path)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("File contetnt: \n", databyte)
+	fmt.Println("File contetnt: \n", string(databyte))
 }
 
-func main() {
-	fmt.Println("File IO")
-	content := "Bull shit"
-
-	file, err := os.Create("file.txt")
+func writeToCsv(path string, data []User) {
+	file, err := os.Create(path)
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
+	for _, user := range data {
+		_, err := file.WriteString(fmt.Sprintf("%s,%d,%s\n", user.Name, user.Age, user.Address))
+		if err != nil {
+			panic(err)
+		}
+	}
+}
 
-	io.WriteString(file, content)
+func main() {
+	// fmt.Println("File IO")
+	// content := "Bull shit"
+
+	// file, err := os.Create("file.txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer file.Close()
+
+	// io.WriteString(file, content)
 
 	// os.WriteFile("file.txt", []byte(content), 0644)
+	users := []User{}
+	append(users, User{Name: "Damiel", Age: 25, Address: "40, lorem, ipsum"})
 
-	readFile()
 }
